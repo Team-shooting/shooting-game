692685d4f694990dad408ca44e13975bdee44cfc
foo2810 <anime_117@yahoo.co.jp>
2017-07-07 20:44:18 +0900

    Update fps.h

--- fps.h
+++ fps.h
@@ -7,10 +7,6 @@ class Fps {
 		double fps;
 		const Uint32 mspf = 1000 / 60;
 
-		Uint32 stotalTime = 0;
-		int frameCount = 0;
-		char drawFps[10];
-
 	public:
 		Fps() = default;
 		~Fps() = default;
